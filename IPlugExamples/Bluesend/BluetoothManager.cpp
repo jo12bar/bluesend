28a309578f265d843b477c576db28ec67d1d4f7d
jo12bar <johann.b@telus.net>
2018-04-09 22:14:21 -0700

    Implement connecting, disconnecting, and writing to devices.

--- IPlugExamples/Bluesend/BluetoothManager.cpp
+++ IPlugExamples/Bluesend/BluetoothManager.cpp
@@ -12,15 +12,58 @@ BluetoothManager::BluetoothManager()
 
 BluetoothManager::~BluetoothManager()
 {
+	if (mConnectedToDevice) DisconnectDevice();
 }
 
 std::vector<device> BluetoothManager::Scan()
 {
 	mDevices = mInq->Inquire();
+	if (!mInitScanDone) mInitScanDone = true;
 	return mDevices;
+}
+
+void BluetoothManager::ConnectDevice(int deviceArrayIdx)
+{
+	if (!mInitScanDone || mConnectedToDevice) return;
+
+	mConnectedDevice = mDevices[deviceArrayIdx];
+	int channelID = mInq->SdpSearch(mConnectedDevice.address);
+	mSerialPort = std::unique_ptr<BTSerialPortBinding>(BTSerialPortBinding::Create(mConnectedDevice.address, channelID));
+	mSerialPort->Connect();
+	mConnectedToDevice = true;
+}
+
+void BluetoothManager::DisconnectDevice()
+{
+	if (!mInitScanDone || !mConnectedToDevice) return;
+
+	mSerialPort->Close();
+	mConnectedToDevice = false;
+}
+
+void BluetoothManager::Write(const char *buffer, int length)
+{
+	if (!mInitScanDone || !mConnectedToDevice) return;
+
+	mSerialPort->Write(buffer, length);
 }
 
 std::vector<device> BluetoothManager::GetDevices()
 {
 	return mDevices;
+}
+
+device BluetoothManager::GetConnectedDevice()
+{
+	return mConnectedDevice;
+}
+
+bool BluetoothManager::IsConnectedToDevice()
+{
+	return mConnectedToDevice;
+}
+
+bool BluetoothManager::IsInitScanDone()
+{
+	return mInitScanDone;
 }
--- IPlugExamples/Bluesend/BluetoothManager.h
+++ IPlugExamples/Bluesend/BluetoothManager.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <vector>
 #include "DeviceINQ.h"
+#include "BTSerialPortBinding.h"
 
 class BluetoothManager
 {
@@ -13,9 +14,28 @@ public:
 	// Scan for new devices, and return a vector of found devices.
 	std::vector<device> Scan();
 
+	// Opens a serial connection to the previously-scanned device in the devices
+	// vector corresponding to the passed-in index.
+	void ConnectDevice(int deviceArrayIdx);
+
+	// Disconnect the connected device, by closing the serial connection &
+	// doing other cleanup tasks.
+	void DisconnectDevice();
+
+	// Write a buffer of chars to the connected device, passing in the length.
+	void Write(const char *buffer, int length);
+
 	std::vector<device> GetDevices();
+	device GetConnectedDevice();
+	bool IsConnectedToDevice();
+	bool IsInitScanDone();
+
 private:
 	std::unique_ptr<DeviceINQ> mInq;
+	std::unique_ptr<BTSerialPortBinding> mSerialPort;
 	std::vector<device> mDevices;
+	device mConnectedDevice;
+	bool mConnectedToDevice;
+	bool mInitScanDone;
 };
 
